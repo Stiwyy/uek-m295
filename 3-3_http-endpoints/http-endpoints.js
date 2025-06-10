a3f73d7bdf41a45aa13bf89829ebe0c642060db5
Andrin <andrinrenggli@outlook.com>
2025-06-10 16:17:08 +0200

    /name needed to be implemented

--- 3-3_http-endpoints/http-endpoints.js
+++ 3-3_http-endpoints/http-endpoints.js
@@ -12,6 +12,12 @@ app.get('/zli', (req, res) => {
     res.redirect('https://www.zli.ch');
 })
 
+app.get('/name', (req, res) => {
+    const names =["Max", "Moritz", "Anna", "Lena", "Paul", "Laura", "Tim", "Sophie", "Ben", "Lisa",
+        "Jonas", "Emma", "Leon", "Mia", "Lukas", "Nina", "Felix", "Sarah", "Tom", "Julia"];
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    res.send(`Zufälliger Name: ${randomName}`);
+})
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
