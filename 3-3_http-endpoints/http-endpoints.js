73857dd9c53707c44fd893f6c010cf2d292c9daf
Andrin <andrinrenggli@outlook.com>
2025-06-10 16:14:52 +0200

    /zli needed to be implemented

--- 3-3_http-endpoints/http-endpoints.js
+++ 3-3_http-endpoints/http-endpoints.js
@@ -7,6 +7,11 @@ app.get('/now', (req, res) => {
     const now = new Date();
     res.send(`Aktuelle Zeit: ${now.toLocaleTimeString()}`);
 });
+
+app.get('/zli', (req, res) => {
+    res.redirect('https://www.zli.ch');
+})
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
