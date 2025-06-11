e66f3f3ec797cb5924dd4e53a9275bd5b4344b61
Andrin <andrinrenggli@outlook.com>
2025-06-11 08:53:02 +0200

    /time /names added

---
+++ 4-2_rest/endpoints2.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let names = [];
+
+app.get('/time', (req, res) => {
+    const tz = req.query.tz || 'UTC';
+    try {
+        const now = new Date().toLocaleString('sv-SE', { timeZone: tz });
+        res.status(200).type('text/plain').send(now);
+    } catch (e) {
+        res.status(400).type('text/plain').send('Invalid timezone');
+    }
+});
+
+app.post('/names', (req, res) => {
+    const name = req.body.name;
+    if (!name) {
+        return res.status(400).type('text/plain').send('Name is required');
+    }
+    names.push(name);
+    res.status(201).type('application/json').json({ names });
+});
+
+app.delete('/names', (req, res) => {
+    const name = req.query.name;
+    if (!name) {
+        return res.status(400).type('text/plain').send('Name query parameter required');
+    }
+    names = names.filter(n => n !== name);
+    res.status(204).send();
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
