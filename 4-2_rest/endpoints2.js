3bec5de8af75bfc775206ad7daae6b2184a60360
Andrin <andrinrenggli@outlook.com>
2025-06-11 09:29:26 +0200

    rest of endpoints implemented

--- 4-2_rest/endpoints2.js
+++ 4-2_rest/endpoints2.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 const port = 3000;
 
 const app = express();
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 let names = [];
@@ -18,6 +19,13 @@ app.get('/time', (req, res) => {
     }
 });
 
+app.get('/names', (req, res) => {
+    if (names.length === 0) {
+        return res.status(404).type('text/plain').send('No names found');
+    }
+    res.status(200).type('application/json').json({ names });
+});
+
 app.post('/names', (req, res) => {
     const name = req.body.name;
     if (!name) {
@@ -28,13 +36,39 @@ app.post('/names', (req, res) => {
 });
 
 app.delete('/names', (req, res) => {
-    const name = req.query.name;
+    const name = req.body.name;
     if (!name) {
-        return res.status(400).type('text/plain').send('Name query parameter required');
+        return res.status(400).type('text/plain').send('Name is required');
     }
     names = names.filter(n => n !== name);
     res.status(204).send();
 });
+let me = {}
+
+app.get('/auth', (req, res) => {
+    const auth = req.get('Authorization')
+    if (auth === 'Basic aGFja2VyOjEyMzQ=') {
+        return res.sendStatus(200)
+    }
+    res.sendStatus(401)
+})
+
+app.get('/joke', async (req, res) => {
+    const name = req.query.name || 'Chuck Norris'
+    try {
+        const response = await fetch('https://api.chucknorris.io/jokes/random')
+        const { value } = await response.json()
+        const joke = value.replace(/Chuck Norris/g, name)
+        res.status(200).json({ joke })
+    } catch {
+        res.status(500).send('Error fetching joke')
+    }
+})
+
+app.patch('/me', (req, res) => {
+    Object.assign(me, req.body)
+    res.status(200).json(me)
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
