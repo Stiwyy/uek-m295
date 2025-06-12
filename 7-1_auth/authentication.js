83a51b87200840127a6c3b3f81db165ce45ec6d2
Andrin <andrinrenggli@outlook.com>
2025-06-12 15:39:34 +0200

    fix basic auth

--- 7-1_auth/authentication.js
+++ 7-1_auth/authentication.js
@@ -2,6 +2,29 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.use(express.json());
+
+app.get('/public', (req, res) => {
+	res.status(200).send('Öffentlicher Endpunkt');
+});
+
+app.get('/private', (req, res) => {
+	const auth = req.headers['authorization'];
+	if (!auth) {
+		res.setHeader('WWW-Authenticate', 'Basic realm="staging server"');
+		return res.status(401).send('Nicht autorisiert');
+	}
+	const base64Credentials = auth.split(' ')[1];
+	const credentials = Buffer.from(base64Credentials, 'base64').toString(
+		'ascii'
+	);
+	const [username, password] = credentials.split(':');
+	if (username === 'zli' && password === 'zli1234') {
+		return res.status(200).send('Privater Endpunkt');
+	} else {
+		res.setHeader('WWW-Authenticate', 'Basic realm="staging server"');
+		return res.status(401).send('Nicht autorisiert');
+	}
+});
 app.listen(port, () => {
 	console.log(`Bibliothek-API läuft auf http://localhost:${port}`);
 });
